29465293eb1c4378d1500b35c7c4b72e98fefc01
frostfire45 <frostfire45@outlook.com>
2019-06-21 21:37:18 -0500

    DependencyInjection Completed

---
+++ MyShopByExample/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using MyShop.Core.Models;
+using System.Data.Entity;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection"){ } // Access by reference. 
+
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCatategory { get; set; }
+    }
+}
